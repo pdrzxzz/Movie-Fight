2cc4568da7d58ac5075c78dedb9065caaa3fabf7
Emanuel <emanuelsalgado007@gmail.com>
2024-11-28 19:23:30 -0300

    dropdown interactivity

--- index.js
+++ index.js
@@ -2,31 +2,69 @@ const fetchData = async searchTerm => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'd9835cc5',
-      s: searchTerm
+      s: searchTerm,
     }
   });
 
   if (response.data.Error) {
     return [];
-  }
+  };
 
   return response.data.Search;
+
 };
 
-const input = document.querySelector('input');
+const autocompleteRoot = document.querySelector('.autocomplete');
+autocompleteRoot.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+
+const dropdown = document.querySelector('.dropdown'); //select dropdown to use the is-active class
+const resultsWrapper = document.querySelector('.results'); //select to show the results
 
 const onInput = async event => {
-  const movies = await fetchData(event.target.value);
+  if (event.target.value) { //if there's an input
+    const movies = await fetchData(event.target.value); //search on api
+    resultsWrapper.innerHTML = ''; //clear all past results
 
-  for (let movie of movies) {
-    const div = document.createElement('div');
+    displayDropdown();
+    for (let movie of movies) { //for each movie found
+      if (movie.Poster != 'N/A') { //if movie has an poster image
+        const option = document.createElement('a');
+        option.classList.add('dropdown-item'); //Bulma styling
+        option.innerHTML = `<img src="${movie.Poster}" /> ${movie.Title}`;
+        resultsWrapper.appendChild(option);
+      }
+    };
 
-    div.innerHTML = `
-      <img src="${movie.Poster}" />
-      <h1>${movie.Title}</h1>
-    `;
-
-    document.querySelector('#target').appendChild(div);
+    if (!movies.length && event.target.value) { //if we dont find a movie and there's an input.
+      resultsWrapper.append('No results found!');
+    };
   }
-};
-input.addEventListener('input', debounce(onInput, 500));
+  else { //if there's no input (the user erases all)
+    hideDropdown();
+  }
+}
+
+const input = document.querySelector('input');
+input.addEventListener('input', debounce(onInput, 500))
+
+const displayDropdown = () => {
+  dropdown.classList.add('is-active');
+  document.addEventListener('click', hideDropdown)
+}
+
+const hideDropdown = () => {
+  dropdown.classList.remove('is-active');
+  document.removeEventListener('click', hideDropdown)
+  // if (input.value) {
+  //   input.addEventListener('click', displayDropdown)
+  // }
+}
+
--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-const debounce = (func, delay = 1000) => {
+const debounce = (func, delay = 1000) => { //debounce receives event on args and passes to func, exists to set timeout logic.
   let timeoutId;
   return (...args) => {
     if (timeoutId) {
